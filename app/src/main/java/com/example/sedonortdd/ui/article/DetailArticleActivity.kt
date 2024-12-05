d00441ef3159501f761b1962751c08fbbdc7ec82
Rizki Siraj <jomirwa@gmail.com>
2024-12-05 19:24:53 +0700

    feat: add detailarticle feature

---
+++ app/src/main/java/com/example/sedonortdd/ui/article/DetailArticleActivity.kt
@@ -0,0 +1,41 @@
+package com.example.sedonortdd.ui.article
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.example.sedonortdd.R
+import com.example.sedonortdd.databinding.ActivityDetailArticleBinding
+import com.example.sedonortdd.databinding.ActivityLokasiBinding
+
+class DetailArticleActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailArticleBinding
+    private lateinit var glideRequestManager: RequestManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityDetailArticleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val intent = intent
+        glideRequestManager = Glide.with(this)
+
+        setupContent(intent, glideRequestManager)
+    }
+
+    fun setupContent(intent: Intent, glide: RequestManager) {
+
+        binding.tvTitleDetailArtikel.text = intent.getStringExtra("judul")
+        binding.tvKontenDetailArtikel.text = intent.getStringExtra("konten")
+
+        glide.load(intent.getStringExtra("imageUrl"))
+            .placeholder(R.drawable.loading)
+            .into(binding.ivDetailArtikel)
+    }
+}
